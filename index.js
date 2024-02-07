56a3a201d10d9be60fd59a72b8d80ae1d0e35fdf
Shubham <shubhamvscode@gmail.com>
2024-02-07 10:58:06 +0530

    refactor: minor change - issue: no-id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.json({ msg: "hello world" });
+  res.json({ msg: "hello bhau" });
 });
 
 app.get("/check", (req, res) => {
